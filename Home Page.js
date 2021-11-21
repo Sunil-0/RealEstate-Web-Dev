8867f99bfafd96b9a8defcd57277a1b8dc8d3022
Sunil-0 <ksrsk000@gmail.com>
2021-11-21 19:22:39 +0530

    Home Page

---
+++ Home Page.js
@@ -0,0 +1,18 @@
+document.getElementById('ct').innerHTML = new Date;
+
+var slideIndex = 0;
+function showSlides() {
+  var i;
+  var slides = document.getElementsByClassName("Slides");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";  
+  }
+  slideIndex++;
+  if (slideIndex > slides.length){
+    slideIndex = 1
+    }    
+
+  slides[slideIndex-1].style.display = "block";  
+  setTimeout(showSlides, 2500);
+}
+showSlides();
